b5b9f50cea75eeb4fbf6a2498ddb48ca4e790240
wonAdam <ehdgus5500@gmail.com>
2021-02-17 16:37:21 +0900

    Slice로 변경

--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -25,39 +25,40 @@ namespace Server
             packetId = (ushort)PacketID.PlayerInfoReq;
         }
 
-        public override void Read(ArraySegment<byte> s)
+        public override void Read(ArraySegment<byte> segment)
         {
+            ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
+
             ushort count = 0;
-            ushort size = BitConverter.ToUInt16(s.Array, s.Offset + count);
+
+            ushort size = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
             count += 2;
 
-            if (size != s.Count)
+            if (size != s.Length)
                 return;
 
-            ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            ushort id = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
             count += 2;
 
 
-            playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));
             count += 8;
         }
 
         public override ArraySegment<byte> Write()
         {
-            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+            ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+            Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
 
             bool success = true;
             ushort count = 0;
 
-            //success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.size);
-            count += 2; // 패킷의 사이즈는 나중에 정함
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packetId);
-            count += 2;
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), playerId);
-            count += 8;
-
-            // 패킷의 사이즈
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+            count += sizeof(ushort); // 패킷의 사이즈는 나중에 정함
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), packetId);
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), playerId);
+            count += sizeof(long);
+            success &= BitConverter.TryWriteBytes(s, count);
 
             if (!success)
                 return null;
